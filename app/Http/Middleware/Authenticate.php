57bb65f1e2d234a313f808da5bfffce9021137bf
rakaiskandar <iskandarraka4@gmail.com>
2022-07-25 11:26:31 +0700

    Initial commit

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
+        }
+
+        if (Auth::guard('admin')->check()) {
+
+            return redirect('/dashboard');
+      
+          } else if (Auth::guard('user')->check()) {
+      
+            return redirect('/dashboard');
+            
+          }
+    }
+}
